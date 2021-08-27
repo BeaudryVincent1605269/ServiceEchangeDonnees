0763ae0bbe4c919c4036bfd4ab076bc0476d58ee
Vincent Beaudry <1605269@cstj.qc.ca>
2021-08-27 14:08:39 -0400

    Cours02

--- semaine01/revision.js
+++ semaine01/revision.js
@@ -23,3 +23,50 @@ console.log(`Bonjour mon nom est ${name}, j'ai ${age} ans`);
 }
 
 displayUser('Cotineau' ,11);
+
+// Collection tableau, Liste
+
+const fruits = ['Kiwi','Banane', 'fraise', 'Pamplemousse', 'Mangue'];
+
+console.log(fruits);
+
+for(let fruit of fruits) {
+    console.log(fruit);
+}
+
+fruits.forEach(f => console.log(f));
+
+const sum = (a,b) => a+b;
+
+const result = sum(2,5);
+console.log(result);
+
+const someFruits = fruits.filter(f => f.length > 5 );
+console.log(someFruits);
+
+const numbers = [10,20,30,40];
+const MULTIPLIER = 3;
+
+const products = numbers.map(n => n* MULTIPLIER);
+console.log(products);
+
+/*const test = numbers.map(n => n* MULTIPLIER).filter(n => n > 75).map(n => n + 9);
+console.log(test);*/
+
+numbers.push(50);
+console.log(numbers);
+
+const avenger = {
+    alterEgo:'Peter Parker',
+    hero:'SpiderMan',
+    movies:[{title:'Tony Hawk'},{title:'Tony Hawk The Return'},{title:'Ultimate Tony hawk against the police'}]
+}
+
+console.log(avenger.alterEgo);
+console.log(avenger.movies);
+avenger.movies.forEach(m => console.log(m.title));
+
+if(1 == '1') {
+    
+}
+
