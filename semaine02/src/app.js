4095863c01f8d64942968f0ac171ae76b5bca860
Vincent Beaudry <1605269@cstj.qc.ca>
2021-09-01 11:24:11 -0400

    Cours03

--- semaine02/src/app.js
+++ semaine02/src/app.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import express from 'express';
 
 const app = express();
@@ -8,16 +9,64 @@ app.get('/', (req, res) => {
     res.send('Notre première route express');
 });
 
-app.get('/maths/somme', (req, res) => {
 
-    //console.log(req.query);
+//maths/produit
+//maths/difference
+//maths/quotient
+//maths/reste
+//maths/somme
+app.get('/maths/:operation', (req, res) => {
+
+    const operation = req.params.operation;
+    console.log(operation);
     const a = parseInt(req.query.a, 10);
     const b = parseInt(req.query.b, 10);
-    const somme = a + b;
+    let result = 0;
+
+    switch (operation) {
+        case 'somme':
+            result = a + b;
+            break;
+        case 'difference':
+            result = a - b;
+            break;
+        case 'quotient':
+            result = a / b;
+            break;
+        case 'reste':
+            result = a % b;
+            break;
+        case 'produit':
+            result = a * b;
+            break;
+        default:
+            console.log('Opération non reconnue');
+            res.status(400).end();
+
+
+
+    }
+
 
     res.status(777);
     res.set('Content-Type', 'text/html');
-    res.send(`<b>${somme}</b>`);
+    res.send(`<b>${result}</b>`);
+
+})
+
+
+
+app.get('/date', (req, res) => {
+    //Status
+    // Content-Type
+    // Réponse
+
+
+    res.status(777);
+    res.set('Content-Type', 'text/html');
+
+    const now = dayjs().format('YYYY-MM-DD HH:mm');
+    res.send(now);
 
 })
 
