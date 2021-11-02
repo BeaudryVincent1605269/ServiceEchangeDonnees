cc3c252e5113c1336c92f5eecd8033d836700002
Vincent Beaudry <1605269@cstj.qc.ca>
2021-11-01 22:34:41 -0400

    it work !!!

--- TP2_ATLAS/js/details.js
+++ TP2_ATLAS/js/details.js
@@ -89,33 +89,34 @@ async function getMonstre(url) {
 
         displaySpecimens(monstre.specimens);
     }
+}
 
 
 
-    function displaySpecimens(specimens) {
+function displaySpecimens(specimens) {
 
 
-        specimens.forEach(s => {
-            displayOneSpecimens(s);
-        });
+    specimens.forEach(s => {
+        displayOneSpecimens(s);
+    });
 
 
-    }
+}
 
-    function displayOneSpecimens(s) {
+function displayOneSpecimens(s) {
+    console.log(s);
 
-
-        let monstreHtml = '<tr>';
-        monstreHtml += `<td><img src="./img/affinities/${s.affinity}.png" alt="${s.affinity}" title="${s.affinity}"></td>"`;
-        monstreHtml += `<td>${s.health}</td>`;
-        monstreHtml += `<td>${s.damage}</td>`;
-        monstreHtml += `<td>${s.speed}</td>`;
-        monstreHtml += `<td>${Math.round(s.critical * 100)}%</td>`;
-        monstreHtml += `<td><img src="./img/affinities/${s.talents[0]}.png" alt="${s.talents[0]}" title="${s.talents[0]}">
+    let monstreHtml = '<tr>';
+    monstreHtml += `<td><img src="./img/affinities/${s.affinity}.png" alt="${s.affinity}" title="${s.affinity}"></td>"`;
+    monstreHtml += `<td>${s.health}</td>`;
+    monstreHtml += `<td>${s.damage}</td>`;
+    monstreHtml += `<td>${s.speed}</td>`;
+    monstreHtml += `<td>${Math.round(s.critical * 100)}%</td>`;
+    monstreHtml += `<td><img src="./img/affinities/${s.talents[0]}.png" alt="${s.talents[0]}" title="${s.talents[0]}">
             <img src="./img/affinities/${s.talents[1]}.png" alt="${s.talents[1]}" title="${s.talents[1]}"></td>`;
 
 
-        monstreHtml += `<td>
+    monstreHtml += `<td>
             <img src="./img/elements/${s.kernel[0]}.png" alt="${s.kernel[0]}" title="${s.kernel[0]}" width="30" height="30">
             <img src="./img/elements/${s.kernel[1]}.png" alt="${s.kernel[1]}" title="${s.kernel[1]}" width="30" height="30">
             <img src="./img/elements/${s.kernel[2]}.png" alt="${s.kernel[2]}" title="${s.kernel[2]}" width="30" height="30">
@@ -123,37 +124,36 @@ async function getMonstre(url) {
             <img src="./img/elements/${s.kernel[4]}.png" alt="${s.kernel[4]}" title="${s.kernel[4]}" width="30" height="30"></td>`;
 
 
-        //console.log(s.hash);
+    //console.log(s.hash);
 
-        let couleur = s.hash;
+    let couleur = s.hash;
 
-        let tableauC = new Array();
+    let tableauC = new Array();
 
-        let debut = couleur.substr(0, 2);
+    let debut = couleur.substr(0, 2);
 
-        let fin = couleur.substr(-2);
+    let fin = couleur.substr(-2);
 
-        let groupeC = 0;
+    let groupeC = 0;
 
-        monstreHtml += '<td>';
-        monstreHtml += '<div class="colored-hash">';
-        monstreHtml += `${debut}`;
-        for (let i = 2; i < couleur.length - 2; i = i + 6) {
-            //console.log(`${i} < ${couleur.length - 2}`);
-            tableauC[groupeC] = couleur.substring(i, i + 6);
-            //console.log(tableauC[groupeC]);
-            monstreHtml += `<span class="block" style="color:#${tableauC[groupeC]}; background-color:#${tableauC[groupeC]};">${tableauC[groupeC]}</span>`;
-            groupeC++
-        }
-        monstreHtml += `${fin}`;
-        monstreHtml += `</div></td></tr>`;
-
-
-
-        $('#stats tbody').append(monstreHtml);
-
-
-
-
+    monstreHtml += '<td>';
+    monstreHtml += '<div class="colored-hash">';
+    monstreHtml += `${debut}`;
+    for (let i = 2; i < couleur.length - 2; i = i + 6) {
+        //console.log(`${i} < ${couleur.length - 2}`);
+        tableauC[groupeC] = couleur.substring(i, i + 6);
+        //console.log(tableauC[groupeC]);
+        monstreHtml += `<span class="block" style="color:#${tableauC[groupeC]}; background-color:#${tableauC[groupeC]};">${tableauC[groupeC]}</span>`;
+        groupeC++
     }
+    monstreHtml += `${fin}`;
+    monstreHtml += `</div></td></tr>`;
+
+
+
+    $('#stats tbody').append(monstreHtml);
+
+
+
+
 }
